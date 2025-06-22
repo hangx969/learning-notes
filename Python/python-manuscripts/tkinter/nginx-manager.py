8f96c2fa817bc6b537c276f344913dd3b11fe116
hangx969 <xuhang969@163.com>
2025-06-22 10:10:33 +0800

    2025-06-22 update from HP Fury16

--- Python/python-manuscripts/tkinter/nginx-manager.py
+++ Python/python-manuscripts/tkinter/nginx-manager.py
@@ -32,9 +32,9 @@ def get_nginx_status():
 # 自动更新nginx状态的标签
 def update_status_label():
     status = get_nginx_status()
-    # status_label是后面定义的标签。参数怎么传进来的？
+    # 动态更新状态标签的文本。status_label是一个全局变量，所以可以直接在函数里使用
     status_label.config(text=f"Nginx Status: {status}")
-    # 每5秒更新一次状态
+    # tkinter定时器函数，每5秒再次调用函数
     root.after(5000, update_status_label)
 
 # 启动nginx服务
@@ -67,25 +67,30 @@ def restart_nginx():
 
 # 查看nginx日志
 def view_nginx_logs():
-    output, error = run_system_command("tail -fn 100 /var/log/nginx/access.log")
+    output, error = run_system_command("tail -n 100 /var/log/nginx/access.log")
     if error:
         messagebox.showerror("Error", f"Failed to get nginx log: {error}")
     else:
         # 创建文本框子窗口，把日志放进去
         logs_window = tk.Toplevel(root)
         logs_window.title("Nginx Log")
+        # 在这个窗口中创建一个文本框 (Text)，wrap='word' 意味着在文本过长时会自动换行。
         logs_text = tk.Text(logs_window, wrap='word')
+        # 将 Nginx 日志（即 output）插入到文本框中，tk.END 表示将内容插入到文本框的末尾。
         logs_text.insert(tk.END, output)
+        # 将文本框布局到窗口中，fill='both' 表示文本框会填满窗口的水平和垂直方向，expand=True 文本框会随着窗口的大小变化而扩展。
         logs_text.pack(fill='both', expand=True)
 
 
 if __name__ == '__main__':
-    # 创建GUI主窗口
+    # 创建一个顶层窗口对象 root，这是整个图形界面的主窗口。
     root = tk.Tk()
     root.title("Nginx manager")
 
     # 显示nginx状态的标签
     status_label = tk.Label(root, text="Nginx Status: Checking...", font=('Arial', 12))
+    # pack() 是用来布局组件的方法之一，它把标签添加到窗口中并设置布局
+    # padx=10, pady=10: 这是为标签设置的内边距，padx 是水平方向的间距，pady 是垂直方向的间距，值为 10，表示标签与窗口边缘或其他组件之间有 10 个像素的间距。
     status_label.pack(padx=10, pady=10)
 
     # 创建启动按钮
@@ -106,5 +111,5 @@ if __name__ == '__main__':
 
     update_status_label()
 
-    # 运行主程序，窗口持续显示
+    # 启动 tkinter 的主事件循环，使应用程序保持运行并响应用户输入。
     root.mainloop()
